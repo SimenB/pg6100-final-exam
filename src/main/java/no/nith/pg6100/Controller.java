6cd94ddb4d750f1fbc328ef5b5ed25160c4d9b86
Simen Bekkhus <sbekkhus91@gmail.com>
2015-05-26 13:10:46 +0200

    Login stuff

---
+++ src/main/java/no/nith/pg6100/Controller.java
@@ -0,0 +1,12 @@
+package no.nith.pg6100;
+
+import javax.enterprise.inject.Model;
+import javax.faces.context.FacesContext;
+
+@Model
+public class Controller {
+    public String logout() {
+        FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
+        return "/login.xhtml";
+    }
+}
